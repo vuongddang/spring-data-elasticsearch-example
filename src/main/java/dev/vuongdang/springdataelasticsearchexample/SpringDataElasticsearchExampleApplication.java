25f0736eb6e87658746828a2b90532c835ecc3ea
Vuong Dang <ddvuong@gmail.com>
2020-10-16 17:51:48 +0700

    Initial commit - bootstrap project by start.spring.io

---
+++ src/main/java/dev/vuongdang/springdataelasticsearchexample/SpringDataElasticsearchExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.vuongdang.springdataelasticsearchexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataElasticsearchExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataElasticsearchExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/vuongdang/springdataelasticsearchexample/SpringDataElasticsearchExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vuongdang.springdataelasticsearchexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataElasticsearchExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
